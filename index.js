bea2c165f8924e35c9353d14ff50137a5ffd987e
github username <github email address>
2019-03-12 19:54:25 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function wakeDog(dogName, dogBreed) {
+      console.log(`Wake ${dogName} the ${dogBreed}`)
+      return `Wake ${dogName} the ${dogBreed}`;
+   }
+function leashDog(dogName, dogBreed) {
+      console.log(`Leash ${dogName} the ${dogBreed}`)
+      return `Leash ${dogName} the ${dogBreed}`;
+   }
+function walkToPark(dogName, dogBreed) {
+      console.log(`Walk to the park with ${dogName} the ${dogBreed}`)
+      return `Walk to the park with ${dogName} the ${dogBreed}`;
+   } 
+function throwFrisbee(dogName, dogBreed) {
+      console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`)
+      return `Thrlearnow the frisbee for ${dogName} the ${dogBreed}`;
+   }
+function walkHome(dogName, dogBreed) {
+      console.log(`Walk home with ${dogName} the ${dogBreed}`)
+      return `Walk home with ${dogName} the ${dogBreed}`;
+   }
+function unleashDog(dogName, dogBreed) {
+      console.log(`Unleash ${dogName} the ${dogBreed}`)
+      return `Unleash ${dogName} the ${dogBreed}`;
+   }
+
+let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+console.log(routine)
+
+function exerciseDog(dName, dBreed) {
+   return routine.map(f => f(dName, dBreed));
+}
+
+
+
+
+// function excerciseDog(dogName, dogBreed) {
+//    let dogLife = Array(1);
+//    for (var i = 0; i < routine.length; i++) {
+//       dogLife.push(function("Byron", "poodle"));
+//    }
+//    return dogLife;
+//  }
